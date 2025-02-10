ebcaaba958a145e8168bbaa02b97db14c0b1c82d
yashwanth <yashwanth.kinnu123@gmail.com>
2025-02-10 16:41:07 +0530

    changes1

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite")
 const sqlite3 = require("sqlite3")
+const cors = require("cors")
 const dbpath = path.join(__dirname, "sample.db")
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 let db;
 
